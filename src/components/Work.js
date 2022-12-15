069d6f84ab0e2b75098cc2dcf7872f97a2b86f57
Anthony Infortunio <ainfortunio1@gmail.com>
2022-12-15 17:15:01 -0500

    Add an option for a adding a company name to a work item
    This commit creates a field that allows the user to add a company name
    to their work history items. This was previously missing and was an
    oversight when planning out the webpage.

--- src/components/Work.js
+++ src/components/Work.js
@@ -4,9 +4,12 @@ import WorkContainer from "./WorkContainer";
 import add from "../assets/add.svg";
 import '../styles/Work.css';
 
+//in the work experience section an option needs to be
+//added to add a company name
 class WorkItem{
-  constructor(title,ystart,yend,location,duties){
+  constructor(title,company,ystart,yend,location,duties){
     this.title=title;
+    this.company=company
     this.ystart=ystart;
     this.yend=yend;
     this.location=location;
@@ -18,7 +21,7 @@ export default class Work extends Component{
   constructor(){
     super();
     this.state = {
-      workArray: [new WorkItem('Waiter','2019','2022','New York, New York','Provide great customer service in a fast paced enviornment, seat, take orders, bring out food and bill patrons. Mantain a clean and presentable work area.'), new WorkItem('Web Developer', '2022', 'Present', 'New York, New York', 'Working on the Odin Project! Learned HTML, CSS, Javascript, React and Jest')],
+      workArray: [new WorkItem('Waiter','Odin Resturaunt','2019','2022','New York, New York','Provide great customer service in a fast paced enviornment, seat, take orders, bring out food and bill patrons. Mantain a clean and presentable work area.'), new WorkItem('Web Developer', 'Odin Web Development','2022', 'Present', 'New York, New York', 'Working on the Odin Project! Learned HTML, CSS, Javascript, React and Jest')],
     };
   }
 
@@ -27,7 +30,7 @@ export default class Work extends Component{
       <div className="work">
         <div className="content-title">Work Experience</div>
         <WorkContainer workArray={this.state.workArray} setWorkItem={this.setWorkItem.bind(this)} removeWorkItem={this.removeWorkItem.bind(this)}/>
-        <img className="add" onClick={()=>{this.addWorkItem(new WorkItem('Position Title', 'Stating Year', 'Ending Year', 'City, State', 'Duties'))}} src={add} alt='add a work item' />
+        <img className="add" onClick={()=>{this.addWorkItem(new WorkItem('Position Title','Company Name','Stating Year', 'Ending Year', 'City, State', 'Duties'))}} src={add} alt='add a work item' />
       </div>
     )
   }
@@ -49,11 +52,12 @@ export default class Work extends Component{
     });
   }
 
-  setWorkItem(title, ystart, yend, location, duties, index){
+  setWorkItem(title, company, ystart, yend, location, duties, index){
     //copy the workArray
     let tempArray = this.state.workArray;
     //update the work item at index
     tempArray[index].title=title;
+    tempArray[index].company=company;
     tempArray[index].ystart=ystart;
     tempArray[index].yend=yend;
     tempArray[index].location=location;
--- src/components/WorkContainer.js
+++ src/components/WorkContainer.js
@@ -10,6 +10,7 @@ const WorkContainer = function(props){
           <li key={workItem.id+'-work'}>
             <div className='work-item' id={workItem.id+'-work'}>
               <p className='work-title'>{workItem.title}</p>
+              <p className='work-company'>{workItem.company}</p>
               <p className='work-year'>{workItem.ystart} - {workItem.yend}</p>
               <p className='work-location'>{workItem.location}</p>
               <p className='duties'>{workItem.duties}</p>
@@ -17,15 +18,17 @@ const WorkContainer = function(props){
 
             <form style={{display: 'none'}} className='work-form' id={workItem.id+'-form'}>
               <label>Title: </label>
-              <input value={workItem.title} onChange={(e)=>{handleInputChange(e.target.value,workItem.ystart,workItem.yend,workItem.location,workItem.duties,props.setWorkItem,props.workArray.indexOf(workItem))}}></input>
+              <input value={workItem.title} onChange={(e)=>{handleInputChange(e.target.value,workItem.company,workItem.ystart,workItem.yend,workItem.location,workItem.duties,props.setWorkItem,props.workArray.indexOf(workItem))}}></input>
+              <label>Company: </label>
+              <input value={workItem.company} onChange={(e)=>{handleInputChange(workItem.title,e.target.value,workItem.ystart,workItem.yend,workItem.location,workItem.duties,props.setWorkItem,props.workArray.indexOf(workItem))}}></input>
               <label>Year Start: </label>
-              <input value={workItem.ystart} onChange={(e)=>{handleInputChange(workItem.title,e.target.value,workItem.yend,workItem.location,workItem.duties,props.setWorkItem,props.workArray.indexOf(workItem))}}></input>
+              <input value={workItem.ystart} onChange={(e)=>{handleInputChange(workItem.title,workItem.company,e.target.value,workItem.yend,workItem.location,workItem.duties,props.setWorkItem,props.workArray.indexOf(workItem))}}></input>
               <label>Year End: </label>
-              <input value={workItem.yend} onChange={(e)=>{handleInputChange(workItem.title,workItem.ystart,e.target.value,workItem.location,workItem.duties,props.setWorkItem,props.workArray.indexOf(workItem))}}></input>
+              <input value={workItem.yend} onChange={(e)=>{handleInputChange(workItem.title,workItem.company,workItem.ystart,e.target.value,workItem.location,workItem.duties,props.setWorkItem,props.workArray.indexOf(workItem))}}></input>
               <label>Location: </label>
-              <input value={workItem.location} onChange={(e)=>{handleInputChange(workItem.title,workItem.ystart,workItem.yend,e.target.value,workItem.duties,props.setWorkItem,props.workArray.indexOf(workItem))}}></input>
+              <input value={workItem.location} onChange={(e)=>{handleInputChange(workItem.title,workItem.company,workItem.ystart,workItem.yend,e.target.value,workItem.duties,props.setWorkItem,props.workArray.indexOf(workItem))}}></input>
               <label>Duties: </label>
-              <input value={workItem.duties} onChange={(e)=>{handleInputChange(workItem.title,workItem.ystart,workItem.yend,workItem.location,e.target.value,props.setWorkItem,props.workArray.indexOf(workItem))}}></input>
+              <input value={workItem.duties} onChange={(e)=>{handleInputChange(workItem.title,workItem.company,workItem.ystart,workItem.yend,workItem.location,e.target.value,props.setWorkItem,props.workArray.indexOf(workItem))}}></input>
             </form>
 
             <img className='edit' src={edit} onClick={()=>{toggleForm(workItem.id)}} alt='edit selected work item' />
@@ -37,8 +40,8 @@ const WorkContainer = function(props){
   )
 };
 
-let handleInputChange = function(title, ystart, yend, location, duties, setWorkItem, index){
-  setWorkItem(title,ystart,yend,location,duties,index);
+let handleInputChange = function(title, company, ystart, yend, location, duties, setWorkItem, index){
+  setWorkItem(title,company,ystart,yend,location,duties,index);
 };
 
 let toggleForm = function(id){
